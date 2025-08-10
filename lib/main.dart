6f2f59a57651524e2f0ea45daefd90068cab7a75
Jiko <youstinahabib422@gmail.com>
2025-08-10 18:41:58 +0300

    dark theme + notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/notes_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const NotesView(),
     );
   }
 }
-
