220f8ac0be4a2a9fd5d71986f3147da92b009f68
Jiko <youstinahabib422@gmail.com>
2025-08-19 13:49:39 +0300

    notesListView + floatingActionButton

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
-import 'package:notes_app/widgets/note_item.dart';
+import 'package:notes_app/widgets/notes_listview.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: const Icon(
+          Icons.add,
+          size: 32,
+          color: Colors.black,
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 24),
         child: Column(
           children: [
             SizedBox(
               height: 35,
             ),
             CustomAppbar(),
-            SizedBox(
-              height: 20,
+            Expanded(
+              child: NotesListview(),
             ),
-            NoteItem(),
           ],
         ),
       ),
---
+++ lib/widgets/notes_listview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/note_item.dart';
+
+class NotesListview extends StatelessWidget {
+  const NotesListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 24),
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 8),
+          child: NoteItem(),
+        );
+      },
+    );
+  }
+}
