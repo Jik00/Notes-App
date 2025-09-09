e5b8357affbbc37c2d57415006ed80b94e5865d3
Jiko <youstinahabib422@gmail.com>
2025-09-09 06:02:44 +0300

    format dateTime [dd-MM-yy, hh:mm a]

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -9,8 +10,12 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(String title, String description ) async {
-    final note = NoteModel(title: title, subtitle: description, date: DateTime.now().toString(), id: DateTime.now().toString());
+  addNote(String title, String description) async {
+    final note = NoteModel(
+        title: title,
+        subtitle: description,
+        date: DateFormat('dd-MM-yy, hh:mm a').format(DateTime.now()),
+        id: DateTime.now().toString());
     emit(AddNoteLoading());
     try {
       final box = Hive.box<NoteModel>(kNotesBox);
