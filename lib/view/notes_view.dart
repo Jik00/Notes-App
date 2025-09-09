873f32a08aebbfb83f362b942b04f7fa8e1cbf44
Jiko <youstinahabib422@gmail.com>
2025-09-09 04:11:32 +0300

    provide blocProvider in notesView

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/read_notes_cubit/read_notes_cubit.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
 import 'package:notes_app/widgets/floating_action_button_add_note.dart';
 import 'package:notes_app/widgets/notes_listview.dart';
@@ -10,25 +12,27 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton: FloatingActionButtonAddNote(),   
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 24),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 35,
-            ),
-            CustomAppbar(
-              title: 'Notes',
-            ),
-            Expanded(
-              child: NotesListview(),
-            ),
-          ],
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: const Scaffold(
+        floatingActionButton: FloatingActionButtonAddNote(),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18, vertical: 24),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 35,
+              ),
+              CustomAppbar(
+                title: 'Notes',
+              ),
+              Expanded(
+                child: NotesListview(),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
